70455fe3bb28d4dd540001c3a97fab1cf10f39a4
lindalopru <lindavalentina507@gmail.com>
2025-08-31 12:17:13 -0500

    feature: add password recovery page

--- src/app/passwordRecovery/page.jsx
+++ src/app/passwordRecovery/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { useState } from 'react';
-import Page from '../recovery/page';
 
 const page = () => {
     const [email, setEmail] = useState('');
@@ -12,18 +11,17 @@ const page = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url("/images/login-background.jpg")' }}>
-            <div className="flex flex-col md:flex-row w-full max-w-5xl bg-transparent rounded-2xl overflow-hidden shadow-xl">
-    
+            <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 md:gap-[40px] items-center min-h-screen">
                 {/* Columna Izquierda: Logo y Sigma */}
-                <div className="flex flex-col items-center justify-center p-8 md:w-1/2 bg-transparent">
+                <div className="flex flex-col justify-center items-center h-full p-8 bg-transparent">
                     <div className="bg-[#D9D9D9] w-[186px] h-[186px] md:w-[286px] md:h-[286px] rounded-full flex justify-center items-center">
-                    <span className="text-black text-[33px] md:text-[51px] font-bold">LOGO</span>
+                        <span className="text-black text-[33px] md:text-[51px] font-bold">LOGO</span>
                     </div>
                     <span className="text-white font-bold text-2xl mt-4">SIGMA</span>
                 </div>
 
                 {/* Columna Derecha: Formulario */}
-                <div className="flex flex-col justify-center p-10 md:w-1/2 bg-black/70 text-white rounded-xl shadow-2xl">
+                <div className="bg-black/60 rounded-[16px] px-8 py-10 min-h-auto md:min-h-[70vh] flex flex-col justify-center items-center gap-12">
                     <h2 className="font-bold text-3xl mb-4 text-center">Password Recovery</h2>
                     <p className="mb-8 text-center">
                         Enter your email address and we'll send you a link to reset your password.
@@ -39,7 +37,7 @@ const page = () => {
                             required
                             className="w-full p-3 rounded-lg bg-white text-black text-base mb-6 shadow-md focus:outline-none focus:ring-2 focus:ring-red-400"
                         />
-                        <button type="submit" className="w-full p-4 bg-red-600 text-white font-bold rounded-lg text-lg hover:bg-red-1000 transition">
+                        <button type="submit" className="w-full p-4 bg-red-600 text-white font-bold rounded-lg text-lg hover:bg-red-700 transition">
                             Send
                         </button>
                     </form>
