047b2a7ad18b8d6b37ec7252042eac2a72dfb19c
unknown <fabianchors@gmail.com>
2025-09-05 00:54:41 -0500

    Se integró endpoint de crear role aunque falta que se muestre mensaje de exito

--- src/services/roleService.js
+++ src/services/roleService.js
@@ -4,3 +4,13 @@ export const changeRoleStatus = async (payload) => {
     const { data } = await apiUsers.post("roles/change-rol-status/", payload);
     return data;
 };
+
+export const getPermissions = async () => {
+    const { data } = await apiUsers.get(`roles/permissions/`);
+    return data;
+};
+
+export const createRole = async (payload) => {
+    const { data } = await apiUsers.post("roles/", payload);
+    return data;
+};
