08f5688aa8f5c351fc8f2783700def097a0630cf
Manuel Isaias Navarro Montealegre <manuelinm2015@gmail.com>
2025-09-23 10:22:38 -0500

    feat: implementar vista principal de maquinaria con filtros y tabla de datos

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -17,6 +17,7 @@ import {
   FaHistory,
   FaDesktop,
   FaRssSquare,
+  FaTractor,
 } from "react-icons/fa";
 import { MdSettings } from "react-icons/md";
 import {
@@ -139,18 +140,10 @@ export default function Sidebar({ isOpen, setIsOpen }) {
     },
     { 
       name: "Maquinaria", 
-      icon: <FaCogs />, 
+      icon: <FaTractor />, 
       path: "/machinery",
       permissions: [],
       module: "machinery",
-      sub: [
-        {
-          name: "Gestion de maquinaria",
-          icon: <FaCogs />,
-          path: "/machinery/mainView",
-          permissions: [],
-        }
-      ]
     },
     {
       name: "Mantenimiento",
