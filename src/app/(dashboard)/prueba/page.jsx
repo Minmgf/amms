d4e519bf81a933a52ef865239629f958b2cfe364
unknown <fabianchors@gmail.com>
2025-09-28 20:53:18 -0500

    Se creó el modal de cancelación de mantenimientos programados

--- src/app/(dashboard)/prueba/page.jsx
+++ src/app/(dashboard)/prueba/page.jsx
@@ -1,9 +1,12 @@
 "use client";
 import React, { useState } from "react";
 import UpdateMaintenanceSchedule from "@/app/components/scheduledMaintenance/UpdateMaintenanceSchedule";
+import CancelScheduledMaintenance from "@/app/components/scheduledMaintenance/CancelScheduledMaintenance";
 
 const PruebaPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
+
 
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
@@ -17,8 +20,20 @@ const PruebaPage = () => {
       >
         Visualizar Modal de Mantenimiento
       </button>
+      <button
+        className="mt-5 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        aria-label="Abrir modal de actualización de mantenimiento"
+        onClick={()=>setIsCancelModalOpen(true)}
+      >
+        Cancelar Mantenimiento Programado
+      </button>
       {isModalOpen && (
         <UpdateMaintenanceSchedule onClose={handleCloseModal} />
+      )}
+      {isCancelModalOpen && (
+        <CancelScheduledMaintenance 
+          isOpen={isCancelModalOpen}
+          onClose={() => setIsCancelModalOpen(false)} />
       )}
     </div>
   );
